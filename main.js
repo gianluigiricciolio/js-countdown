47e4b273aafe12c4d5cc774d869fa9d2538d32e9
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-14 11:45:09 +0200

    [UPD] button

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 
 
 let maxSeconds=11;
+let isPressed=false;
+const btn=document.querySelector('.start');
 
-// SET INTERVAL HA UN IDENTIFICATIVO (COSI COME SET TIMEOUT) CHE POSSIAMO USARE PER STOPPARE QUEL PARTICOLARE EVENTO CON CLEAR INTERVAL (CLOCK)
-const clock = setInterval(countDown,1000);
-setTimeout(stopCountDown,maxSeconds*1000);
+btn.addEventListener("click", function(){
+    if(!isPressed) startCountDown();
+    isPressed=true;
+})    
+
+
+
+function startCountDown() {
+    // SET INTERVAL HA UN IDENTIFICATIVO (COSI COME SET TIMEOUT) CHE POSSIAMO USARE PER STOPPARE QUEL PARTICOLARE EVENTO CON CLEAR INTERVAL (CLOCK)
+    const clock = setInterval(countDown,1000);
+    setTimeout(stopCountDown,maxSeconds*1000);
+}
 
 function countDown() {
     maxSeconds--;
