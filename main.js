35ab1648fb6f336aecfd90de7753ddb3a991d5da
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-14 10:27:20 +0200

    [UPD] created a stopCountDown to use the setTimeout function

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 'use strict';
 
 
-let maxSeconds=10;
-
-function countdown() {
-    if(maxSeconds===0){
-        console.log('Buon anno!');
-        // INTERROMPO L INTERVALLO
-        clearInterval(clock);
-    }
-    else{
-        console.log(maxSeconds);
-        maxSeconds--;
-    }
-}
+let maxSeconds=11;
 
 // SET INTERVAL HA UN IDENTIFICATIVO (COSI COME SET TIMEOUT) CHE POSSIAMO USARE PER STOPPARE QUEL PARTICOLARE EVENTO CON CLEAR INTERVAL (CLOCK)
-const clock = setInterval(countdown,1000);
+const clock = setInterval(countDown,1000);
+setTimeout(stopCountDown,maxSeconds*1000);
+
+function countDown() {
+    maxSeconds--;
+    console.log(maxSeconds);
+    
+}
+
+function stopCountDown() {
+    console.log('Buon anno!');
+    // INTERROMPO L INTERVALLO
+    clearInterval(clock);
+}
